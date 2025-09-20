1d812852327ccf80cc279375975bbc80fbf45577
hoenas <hoenas@gmail.com>
2025-09-20 13:52:34 +0200

    Added zsh config

--- home.nix
+++ home.nix
@@ -14,12 +14,26 @@
   };
 
   programs.zsh = {
-    # Your zsh config
+    enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history.size = 10000;
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+      ];
+    };
     oh-my-zsh = { # "ohMyZsh" without Home Manager
       enable = true;
-      plugins = [ "git" "thefuck" ];
+      plugins = [ "git"];
       theme = "powerlevel10k";
     };
   };
+
 
 }
